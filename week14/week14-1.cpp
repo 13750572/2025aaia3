9685b1e6833fe9778c3243242796507c8c24e041
13750572 <stanley99532@gmail.com>
2025-12-11 17:04:23 +0800

    week14

---
+++ week14/week14-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        vector<int> a;
+        while(head !=nullptr){
+            a.push_back(head->val);
+            head=head->next;
+        }
+        ListNode* ans=new ListNode(999);
+        ListNode* ans2=ans;
+        for(int i=a.size()-1;i>=0;i--){
+            ans2->next=new ListNode( a[i] );
+            ans2=ans2->next;
+        }
+        return ans->next;
+    }
+};
